18a98de656659b14487156bc7f12be87a9eb4002
Wannes Salomé <mail@wannessalome.nl>
2021-11-09 10:24:05 +0100

    feat: add spotify to socials

---
+++ src/lib/socials.ts
@@ -0,0 +1,26 @@
+import type { IconType } from 'react-icons';
+import { FaFacebook, FaInstagram, FaSpotify } from 'react-icons/fa';
+
+interface Social {
+  name: string;
+  href: string;
+  Icon: IconType;
+}
+
+export const socials: Social[] = [
+  {
+    name: 'Facebook',
+    href: 'https://www.facebook.com/thisisloupe',
+    Icon: FaFacebook,
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/thisisloupe/',
+    Icon: FaInstagram,
+  },
+  {
+    name: 'Spotify',
+    href: 'https://open.spotify.com/artist/23n2oObsIrvqtcOVwhyT3o?si=j-sz9MnoSEeFyQDzpJa7Gw',
+    Icon: FaSpotify,
+  },
+];
