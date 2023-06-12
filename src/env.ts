a544985e021fa1ab2f85e7c0f1e618ae44df7e89
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2023-06-12 22:15:12 +0200

    fix: fix env types

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@
 import { parseEnv, z } from 'znv';
 
 export const env = parseEnv(import.meta.env, {
-  ABOSS_AGENCY_ID: z.string(),
-  ABOSS_ARTIST_ID: z.string(),
+  ABOSS_AGENCY_ID: z.string().or(z.number()),
+  ABOSS_ARTIST_ID: z.string().or(z.number()),
   ABOSS_TOKEN: z.string(),
 });
